d4276df2e4e74c6d81e156b9943b9704eb11138a
TheAwesomePossum <pi@raspberrypi.(none)>
2014-04-02 16:57:44 -0600

    all done except sonar drivers, added main program

---
+++ robot.py
@@ -0,0 +1,16 @@
+'''
+Author: Graham Montgomery
+
+This is the main program for the robot, that will be set to run on boot.
+We will move this time!
+'''
+
+# import engine
+from Engine import *
+
+
+# define Motors
+
+rMotor = Motor('51FF-7106-4980-4956-5739-1387')
+lMotot = Motor('51FF-6F06-4980-4956-2739-1387')
+
